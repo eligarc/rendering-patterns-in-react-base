f21848631681e999e4cc32fb97f5ecb9fabe484f
eliogrc <elioenaig041294@gmail.com>
2025-01-28 15:03:31 -0600

    Render props

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,18 @@
-export const ParentComponent = () => {
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element
+}
+
+export const ChildComponent: React.FC<ChildComponentProps> = ({ render }) => {
+  const name = "Eliogrc";
+
   return (
-    <>
-    </>
+    <div>
+    {render(name)}
+    </div>
   );
+};
+
+
+export const ParentComponent = () => {
+  return (<ChildComponent render={(name) => <p>Hello, {name} !</p>} />);
 };
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,16 @@
-const ParentComponent = () => {
+import { useState } from "react";
+
+interface ParentComponentProps {
+  render: (data: string[]) => React.ReactNode
+}
+
+const ParentComponent: React.FC<ParentComponentProps> = ({ render }) => {
+  const [data] = useState<string[]>(["Apple 🍎", "Banana 🍌", "Orange 🍊"]);
+  return (
+    <ul>
+      {render(data)}
+    </ul>
+  );
 };
 
 export default ParentComponent;

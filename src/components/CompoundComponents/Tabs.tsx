62b5094de7e8320d427a623546f6be6a3339f9a5
eliogrc <elioenaig041294@gmail.com>
2025-01-31 10:38:42 -0600

    Patron component Compound

--- src/components/CompoundComponents/Tabs.tsx
+++ src/components/CompoundComponents/Tabs.tsx
@@ -1,8 +1,43 @@
+import { ReactElement, ReactNode, useState } from "react";
+import React from "react";
+
 import classes from "./CompoundComponents.module.css";
 
-const Tabs = () => {
+interface TabProps {
+  children: ReactNode
+}
+
+// interface TabProps extends React.PropsWithChildren {
+//   label: string;
+// }
+
+const Tabs: React.FC<TabProps> = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  const tabElements = React.Children.toArray(children).filter(
+    (child): child is ReactElement => React.isValidElement(child)
+  );
+
   return (
     <div className={classes.Tabs}>
+      <ul>
+        {tabElements.map((child, index) => (
+          <li
+            key={index}
+            className={`${index === activeIndex ? classes.TabActive : ""}`}
+            onClick={() => handleTabClick(index)}
+          >
+            {child.props.label}
+          </li>
+        ))}
+      </ul>
+      <p className={classes.TabContent}>
+        {tabElements[activeIndex]}
+      </p>
     </div>
   );
 };

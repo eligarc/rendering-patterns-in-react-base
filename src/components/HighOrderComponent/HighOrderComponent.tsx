2ba54ff2b41dc199d7aa723ef3802c17d28c566e
eliogrc <elioenaig041294@gmail.com>
2025-01-31 13:48:41 -0600

    High-OrderComponents [class 5]

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,25 @@
+function withLoading<T extends object>(Component: React.ComponentType<T>) {
+  return (props: T & { isLoading: boolean }) => {
+    const { isLoading, ...rest } = props;
+    return isLoading ? <p>Loading...</p> : <Component {...rest as T}/>
+  }
+}
+
+type UserProps = {
+  name: string
+}
+
+const UserCompoent: React.FC<UserProps> = ({ name }) => {
+  return <p>Hello, {name} !</p>
+}
+
+const UserWithLoading =  withLoading(UserCompoent);
+
 export const ParentComponent = () => {
   return (
     <div>
+      <UserWithLoading isLoading={true} name="Eliogrc" />
+      <UserWithLoading isLoading={false} name="Eliogrc" />
     </div>
   );
 };

41688ec2b5a54c00bbcdc32dae5eb1332eacd553
eliogrc <elioenaig041294@gmail.com>
2025-02-11 14:34:14 -0600

    Controlled & Uncontrolled component

--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,20 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const hanldeSubmint = () => {
+    const value = inputRef.current?.value;
+    if (value) {
+      console.log(`Producto agregado: ${value}`);
+      alert(`Producto agregado: ${value}`);
+    }
+  }
+
   return (
     <div>
+      <input type="text" ref={inputRef} placeholder="Nombre del producto. ej. manzana" />
+      <button onClick={hanldeSubmint}>Agregar al carrito</button>
     </div>
   );
 };

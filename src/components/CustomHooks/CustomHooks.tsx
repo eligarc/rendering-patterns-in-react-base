9c7d441f4bd36d08d647abdd4f55845b2a014673
eliogrc <elioenaig041294@gmail.com>
2025-02-10 22:56:10 -0600

    Custom hook Loading

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,33 @@
-const UserComponent = () => {
+import { useState } from "react";
+
+function useLoading(initialLoading: boolean = false) {
+  const [loading, setLoading] = useState(initialLoading);
+
+  return {
+    loading,
+    setLoading,
+  }
+}
+
+type userProps = {
+  name: string;
+}
+const UserComponent: React.FC<userProps> = ({ name }) => {
+  const { loading, setLoading } = useLoading(false);
+
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
+      <p>Hello, { name } !</p>
+      <button onClick={() => setLoading(true)}>Simulate loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name='eliogrc' />;
 };
